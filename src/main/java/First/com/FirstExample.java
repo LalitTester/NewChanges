7b2024da9a7d8feab4e513ce8f9a7b254d28548d
LalitTester <lalitkumar2707@gmail.com>
2023-05-11 08:40:13 +0530

    Changes in first examples

--- src/main/java/First/com/FirstExample.java
+++ src/main/java/First/com/FirstExample.java
@@ -3,6 +3,7 @@ package First.com;
 public class FirstExample {
     public static void main(String args[]){
         System.out.println("Hiiiiiiii");
+        System.out.println("How are you...........!!!!");
     }
 
 
